e42da0bfbe315cc462cdd54d4e2104dac1463498
xannor <xannor@bitsonthefloor.com>
2020-12-28 21:26:02 +0000

    Added missing STREAM_SUPPORT

--- custom_components/shinobi/camera.py
+++ custom_components/shinobi/camera.py
@@ -17,8 +17,13 @@ import logging
 from haffmpeg.camera import CameraMjpeg
 from haffmpeg.tools import IMAGE_JPEG, ImageFrame
 
-from homeassistant.components.camera import Camera, DEFAULT_CONTENT_TYPE
-from homeassistant.components.ffmpeg import DATA_FFMPEG
+from homeassistant.components.camera import (
+    SUPPORT_ON_OFF,
+    SUPPORT_STREAM,
+    Camera,
+    DEFAULT_CONTENT_TYPE,
+)
+from homeassistant.components.ffmpeg import CONF_EXTRA_ARGUMENTS, DATA_FFMPEG
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
 from .const import DOMAIN, CAMERA_WEB_SESSION_TIMEOUT
@@ -39,7 +44,7 @@ async def async_setup_entry(
 
     cams = []
     for monitor in monitors:
-        cams.append(ShinobiCamera(entry.entry_id, hass.data[DATA_FFMPEG], monitor))
+        cams.append(ShinobiCamera(entry.entry_id, monitor))
 
     async_add_entities(cams)
 
@@ -47,12 +52,12 @@ async def async_setup_entry(
 class ShinobiCamera(ShinobiEntityMixin[Monitor], Camera):
     """An Implementation of a Shinobi Camera """
 
-    def __init__(self, config_entry_id, ffmpeg, monitor: Monitor):
+    def __init__(self, config_entry_id, monitor: Monitor):
         """ Initialize a Shinobi Camera """
         super(ShinobiCamera, self).__init__(config_entry_id, monitor, "monitors")
         self._name = monitor.name
         self._content_type = monitor.type or DEFAULT_CONTENT_TYPE
-        self._ffmpeg = ffmpeg
+        self._supported_features = 0
         _LOGGER.debug("Initialized: %s" % monitor)
 
     async def async_added_to_hass(self):
@@ -64,6 +69,7 @@ class ShinobiCamera(ShinobiEntityMixin[Monitor], Camera):
         else:
             self._stream_mjpeg_source = None
         self._stream_source = next(iter(self._device.streams), None)
+        self._supported_features = SUPPORT_STREAM if self._stream_source else 0
         self._last_image = None
 
     async def async_will_remove_from_hass(self):
@@ -74,6 +80,11 @@ class ShinobiCamera(ShinobiEntityMixin[Monitor], Camera):
     def name(self):
         """ return the name of this Camera """
         return self._name
+
+    @property
+    def supported_features(self):
+        """Return supported features for this camera."""
+        return self._supported_features
 
     @property
     def unique_id(self):
@@ -88,7 +99,10 @@ class ShinobiCamera(ShinobiEntityMixin[Monitor], Camera):
 
     async def async_create_still_from_stream(self):
         """ Generate a still image from camera stream using FFMPEG """
-        ffmpeg = ImageFrame(self._ffmpeg.binary)
+
+        ffmpeg_manager = self.hass.data[DATA_FFMPEG]
+
+        ffmpeg = ImageFrame(ffmpeg_manager.binary)
 
         if self._stream_source is None:
             return
@@ -132,7 +146,31 @@ class ShinobiCamera(ShinobiEntityMixin[Monitor], Camera):
         if self._stream_source is None:
             return None
 
-        return self._stream_source
+        return self._stream_source.url
+
+    async def async_create_mjpeg_from_stream(self, request):
+        """ Create MJPEG from string"""
+
+        _LOGGER.debug(
+            "converting source stream to mjpeg for replay from %s", self._name
+        )
+        ffmpeg_manager = self.hass.data[DATA_FFMPEG]
+
+        streaming_url = self._stream_source.url
+        stream = CameraMjpeg(ffmpeg_manager.binary)
+        await stream.open_camera(streaming_url)  # , extra_cmd=self._ffmpeg_arguments)
+
+        try:
+            stream_reader = await stream.get_reader()
+            return await async_aiohttp_proxy_stream(
+                self.hass,
+                request,
+                stream_reader,
+                ffmpeg_manager.ffmpeg_stream_content_type,
+            )
+        finally:
+            _LOGGER.debug("Closing MJPEG stream from %s" % self._name)
+            await stream.close()
 
     async def handle_async_mjpeg_stream(self, request):
         """Return an MJPEG stream."""
@@ -150,18 +188,4 @@ class ShinobiCamera(ShinobiEntityMixin[Monitor], Camera):
 
             return await async_aiohttp_proxy_web(self.hass, request, stream_coro)
 
-        _LOGGER.debug("converting source stream to mjpeg for replay")
-        streaming_url = self._stream_source.url
-        stream = CameraMjpeg(self._ffmpeg.binary)
-        await stream.open_camera(streaming_url)  # , extra_cmd=self._ffmpeg_arguments)
-
-        try:
-            stream_reader = await stream.get_reader()
-            return await async_aiohttp_proxy_stream(
-                self.hass,
-                request,
-                stream_reader,
-                self._ffmpeg.ffmpeg_stream_content_type,
-            )
-        finally:
-            await stream.close()
+        return await self.async_create_mjpeg_from_stream(request)
